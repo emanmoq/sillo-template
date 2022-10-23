c1c6e58f30753cbc3f8bb663ca5aba64c7ca19d0
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-10-23 10:25:07 -0700

    xcxc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 jQuery(function ($) {
-
+    new WOW().init();
+    
+  $(".readOnlyRating").starRating({
+    starSize: 10,
+    activeColor: '#FFB503',
+    hoverColor: '#FFD93F',
+    ratedColor: '#FFD93F',
+    emptyColor: '#CECECE',
+    starShape: 'straight',
+    strokeColor:'#FFD93F',
+    strokeWidth:9,
+    useGradient: false,
+    readOnly: true,
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
 });
 
