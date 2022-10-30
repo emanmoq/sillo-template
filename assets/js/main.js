83896d9e0ada3450e5757c85a5642c18477af93c
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-10-29 17:40:32 -0700

    shop page

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,21 @@ $(document).ready(function () {
       // make a server call here
     }
   });
+  $(".FilterRating").starRating({
+    starSize:20,
+    activeColor: '#FF8A00',
+    hoverColor: '#FFD93F',
+    ratedColor: '#FF8A00',
+    emptyColor: '#CCCCCC',
+    starShape: 'straight',
+    strokeColor: '#FFD93F',
+    strokeWidth: 9,
+    useGradient: false,
+    readOnly: true,
+    callback: function (currentRating, $el) {
+      // make a server call here
+    }
+  });
   $(".OurPartnersCar").owlCarousel({
     nav: false,
     loop: true,
@@ -103,5 +118,20 @@ $(document).ready(function () {
       }
     }
   });
+  $("#price-range").slider({
+    step: 10,
+    range: true, 
+    min: 0, 
+    max: 7000, 
+    values: [150, 3000], 
+    
+    slide: function(event, ui)
+    {$(".priceRange").val(ui.values[0] + " - " + ui.values[1]);}
+  });
+  $(".priceRange").val($("#price-range").slider("values", 0) + " - " + $(".price-range").slider("values", 1));
+$(".FilterList li a").click(function(){
+  $(this).parent().find(".filterContent").slideToggle();
+  $(this).find('.fa-solid').toggleClass('fa-solid fa-chevron-up  fa-solid fa-chevron-down');
+});
 });
 
